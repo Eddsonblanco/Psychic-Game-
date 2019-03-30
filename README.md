86d8718eba0913d079c1692b19fe3b7d6dd10a00
eddsonblanco <eddsonrbr@gmail.com>
2019-03-30 14:05:07 -0700

    finally done

--- README.md
+++
@@ -1,1 +0,0 @@
-https://www.youtube.com/watch?v=qTc45Lox97g&feature=youtu.be    <-----  esto necesito hacer hahah
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,55 @@
-alert("hola mundo");
+//Set up all the needed variables
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 5;
+var currentGuesses = "";
+
+//Write an algorithm that generates a random letter
+
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var randomLetter = alphabet[Math.floor(Math.random() * (alphabet.length -1))];
+
+
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+    
+
+    if (userGuess === randomLetter) {
+        wins++;
+        guessesLeft = 5;
+        currentGuesses = "";
+        alert("you win!");
+    }
+    else if (guessesLeft === 1){
+        losses++;
+        alert("you lose!");
+        currentGuesses = "";
+        guessesLeft = 5;
+    }
+    else {
+        guessesLeft--;
+        currentGuesses += userGuess + ",";
+    }
+
+    //Update all the HTML parts to show the user
+    var element = document.getElementById("wins");
+    element.innerHTML = "Wins: " + wins;
+
+    var element = document.getElementById("losses");
+    element.innerHTML = "Losses: " + losses;
+
+    var element = document.getElementById("guesses-left");
+    element.innerHTML = "Guesses left: " + guessesLeft;
+
+    var element = document.getElementById("guesses-current");
+    element.innerHTML = "Your Guesses So Far: " + currentGuesses;
+
+
+//    debuggin purposes, 
+    console.log("CPU letter: " + randomLetter);
+    console.log("Wins: " + wins);
+    console.log("Losses: " + losses);
+    console.log("Guesses Left: " + guessesLeft);
+    console.log("Guesses so far: " + currentGuesses);
+}
